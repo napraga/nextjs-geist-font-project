5dd0ecc7e706cf4200f78a86276851f35d699c13
Alexander Alvarez <alexander@desarrollador.info>
2025-07-27 14:42:55 -0500

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1753645374727

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,14 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_next_dist_c3a575e9._.js",
+  "static/chunks/node_modules_@firebase_auth_dist_esm_b9acc9cd._.js",
+  "static/chunks/node_modules_@firebase_database_dist_index_esm_83804e9a.js",
+  "static/chunks/node_modules_@firebase_storage_dist_index_esm_752d57ce.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_98dc76df._.js",
+  "static/chunks/src_09842459._.js"
+],
+    source: "dynamic"
+});
