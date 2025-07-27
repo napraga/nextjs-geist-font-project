738d5d85309463980965e3575abe7ee58f50c288
Alexander Alvarez <alexander@desarrollador.info>
2025-07-27 14:43:06 -0500

    Add .next/static/chunks/src_app_page_tsx_9d8b0708._.js 1753645386393

---
+++ .next/static/chunks/src_app_page_tsx_9d8b0708._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_1c81c62a._.js"
+],
+    source: "dynamic"
+});
